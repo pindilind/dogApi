880642ef6b1976e1bfaae52317b326db5d051516
pindilind <linda.gustafsson@outlook.com>
2021-09-02 09:08:23 +0200

    gitignore

--- public/public.js
+++ public/public.js
@@ -31,15 +31,12 @@ boneIcon.style.color = "#fff3b0";
 boneIcon.style.zIndex = "-1"
 
 getDogBtn.style.zIndex = "2"
-/* getDogBtn.style.width = "200px" */
 getDogBtn.style.height = "30px"
 getDogBtn.style.justifyContent = "center"
 getDogBtn.style.alignItems = "center"
 getDogBtn.style.display = "flex"
 getDogBtn.style.fontFamily = "Wire One";
 getDogBtn.style.fontSize = "30px";
-/* getDogBtn.style.padding= "0px";
-getDogBtn.style.borderRadius = "2px"; */
 getDogBtn.style.border = "none";
 getDogBtn.style.backgroundColor = "#fff3b0";
 getDogBtn.style.color = "#9e2a2b";
@@ -50,13 +47,17 @@ bodyDiv.appendChild(containerOne)
 containerOne.appendChild(boneIcon)
 containerOne.appendChild(getDogBtn)
 getDogBtn.appendChild(getDogText)
-/* getDogBtn.appendChild(boneIcon)  */
+
 
 getDogBtn.onclick = async function() { //onclick funktion
+
+    document.getElementById('imgDiv').innerHTML = ""; //kod rensa div
     try {
     let response = await fetch("https://dog.ceo/api/breeds/image/random")
-        let body = await response.json()
+      let body = await response.json()
         console.log(body)
+
+        document.getElementById('imgDiv').innerHTML = ""; //kod rensa div
         
         let img = document.createElement('img')
         img.style.maxHeight = "300px"
@@ -69,7 +70,6 @@ getDogBtn.onclick = async function() { //onclick funktion
 
        document.getElementById('imgDiv').appendChild(img)
 
-       /* document.getElementById('imgDiv').innerHTML = ""; */ //kod rensa div
        
     } catch(err) {
         console.log(err)
@@ -80,11 +80,20 @@ getDogBtn.onclick = async function() { //onclick funktion
     } 
 
 
- let btnDiv = document.createElement("div")
+let btnDiv = document.createElement("div")
+
 
 let saveTodoBtn = document.createElement('button') //skapar knapp
 let saveTodoText = document.createElement('h4') // skapar h4
-saveTodoText.innerText = "Spara" //skriver text på knapp
+saveTodoText.innerText = "SAVE" //skriver text på knapp
+
+saveTodoBtn.style.border = "none";
+saveTodoBtn.style.backgroundColor = "#fff3b0";
+saveTodoBtn.style.color = "#9e2a2b";
+saveTodoBtn.style.marginTop = "-20px";
+saveTodoBtn.style.fontFamily = "Wire One";
+saveTodoBtn.style.fontSize = "25px";
+
 
 containerTwo.appendChild(btnDiv)
 btnDiv.appendChild(saveTodoBtn)
@@ -102,17 +111,29 @@ saveTodoBtn.onclick = async function() { //onclick funktion
 
 let getToDoBtn = document.createElement('button') //skapar knapp
 let getToDoText = document.createElement('h4') // skapar h4
-getToDoText.innerText = 'Hämtar ToDo' //skriver text på knapp
+getToDoText.innerText = 'GET TODO' //skriver text på knapp
 
 
 containerTwo.appendChild(btnDiv)
 btnDiv.appendChild(getToDoBtn)
 getToDoBtn.appendChild(getToDoText)
 
+getToDoBtn.style.border = "none";
+getToDoBtn.style.backgroundColor = "#fff3b0";
+getToDoBtn.style.color = "#9e2a2b";
+getToDoBtn.style.marginTop = "-72px";
+getToDoBtn.style.marginLeft = "0px";
+getToDoBtn.style.fontFamily = "Wire One";
+getToDoBtn.style.fontSize = "25px";
+
+
+
     getToDoBtn.onclick = async function() { //onclick funktion 
 
         const displayToDo = await makeRequest("http://localhost:3000/api", "GET")
         console.log(displayToDo)
+
+        document.getElementById('toDoDiv').innerHTML = "";
 
         for (let i = 0; i < displayToDo.length; i++) {
             const element = document.createElement('li')
